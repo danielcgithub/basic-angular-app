64f51b7b7b3f7dac948452453b92c9fe15d854c6
Daniel <d.cunniffe.ie@gmail.com>
2019-04-19 16:02:12 +0100

    You refactored data access to the HeroService class. You registered the HeroService as the provider of its service at the root level so that it can be injected anywhere in the app. You used Angular Dependency Injection to inject it into a component.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+
 
 
 @Component({
@@ -9,21 +10,30 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+
+  constructor(private heroService: HeroService) {
+   }
+
+   ngOnInit() {
+    this.getHeroes();
+  }
+
   hero : Hero = {
     id: 1,
     name: 'Windstorm'
   };
+
+  heroes: Hero[];
 
   selectedHero: Hero;
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
 
-  heroes = HEROES;
-
-  constructor() { }
-
-  ngOnInit() {
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
   }
+
+  
 
 }
